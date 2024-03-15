234efa7106902b5f1e99b830869a48af92edf9a4
Dylan Beattie <dylan@dylanbeattie.net>
2024-03-15 17:42:30 +0000

    Blazor POC is working - cool!

--- Engine/Class1.cs
+++ Engine/Class1.cs
@@ -1,6 +1,6 @@
 namespace Engine;
 
-public class Engine {
+public class Interpreter {
 	public string Run(string program) {
 		return $"{program} It worked - ran {program.Length} characters at {DateTime.UtcNow:O}";
 	}
