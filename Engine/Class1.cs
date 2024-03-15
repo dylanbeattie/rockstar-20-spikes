c60dec4c1d46441ecd40b47121479a0253750584
Dylan Beattie <dylan@dylanbeattie.net>
2024-03-15 18:25:34 +0000

    Working AOT command line app on Windows

--- Engine/Class1.cs
+++ Engine/Class1.cs
@@ -1,7 +1,11 @@
+using System.Globalization;
+
 namespace Engine;
 
 public class Interpreter {
 	public string Run(string program) {
-		return $"{program} It worked - ran {program.Length} characters at {DateTime.UtcNow:O}";
+		var parser = new PegExamples.ExpressionParser();
+		var result = parser.Parse(program);
+		return result.ToString(CultureInfo.InvariantCulture);
 	}
 }
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,4 +1,4 @@
 var program = File.ReadAllText(args[0]);
-var engine = new Engine.Engine();
+var engine = new Engine.Interpreter();
 var result = engine.Run(program);
 Console.WriteLine(result);
